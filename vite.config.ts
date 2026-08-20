d8555e87cdd66e52ba810321b2bccf2c0cf76438
GreatFurby <rubenverhulst@gmail.com>
2026-08-20 10:15:07 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/eli-tracker/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
